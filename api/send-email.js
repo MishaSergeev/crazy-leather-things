1099e4b4c755cb923299f178b8edf9c450c81708
Mikhail Sergeev <sergeev.mikhail.serg@gmail.com>
2025-08-14 21:15:44 +0300

    send-email  update

--- api/send-email.js
+++ api/send-email.js
@@ -9,34 +9,35 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_APP_PASSWORD,
-  },
-});
-
 app.post('/send-email', async (req, res) => {
-  const { email, subject, html } = req.body;
+    const { email, subject, html } = req.body;
 
-  if (!email || !subject || !html) {
-    return res.status(400).json({ error: 'Все поля обязательны' });
-  }
-
-  try {
-    const info = await transporter.sendMail({
-      from: process.env.GMAIL_USER,
-      to: email,
-      subject,
-      html: html,
+    if (!email || !subject || !html) {
+        return res.status(400).json({ error: 'Все поля обязательны' });
+    }
+    if (req.method !== 'POST') {
+        return res.status(405).json({ error: 'Method Not Allowed' });
+    }
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.GMAIL_USER,
+            pass: process.env.GMAIL_APP_PASSWORD,
+        },
     });
+    try {
+        const info = await transporter.sendMail({
+            from: process.env.GMAIL_USER,
+            to: email,
+            subject,
+            html: html,
+        });
 
-    res.json({ id: info.messageId, accepted: info.accepted });
-  } catch (err) {
-    console.error('Error:', err);
-    res.status(500).json({ error: 'Error sending email' });
-  }
+        res.json({ id: info.messageId, accepted: info.accepted });
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).json({ error: 'Error sending email' });
+    }
 });
 
 const PORT = 5000;
